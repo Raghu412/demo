9a9381d0414abae47ed13f02ba3a197fb39fadd8
Raghu412 <matrixbender.412@gmail.com>
2020-07-18 21:44:39 +0530

    Updates

---
+++ precache-manifest.f0b2148e91138527d38595b68e45068c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99eb0ca560ffe11047893351d908d8b6",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "0a7928b571b4268a128e",
+    "url": "/demo/static/css/main.fbd7d71b.chunk.css"
+  },
+  {
+    "revision": "8a5b62b8bce7bc325738",
+    "url": "/demo/static/js/2.7a2e5b69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/demo/static/js/2.7a2e5b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7928b571b4268a128e",
+    "url": "/demo/static/js/main.74d5ac06.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  },
+  {
+    "revision": "20b5b8d9a848576cb406917294e65837",
+    "url": "/demo/static/media/Agriculture Produce.20b5b8d9.png"
+  },
+  {
+    "revision": "9f79b3c7e173924f650eae35e486cfcd",
+    "url": "/demo/static/media/Granite.9f79b3c7.png"
+  },
+  {
+    "revision": "e48c6f6cdee1a4a2a4ad4a4ec72a6f25",
+    "url": "/demo/static/media/Marble.e48c6f6c.png"
+  },
+  {
+    "revision": "3ce41b879f6fe86aadde3532b5d19f59",
+    "url": "/demo/static/media/Natural Stones.3ce41b87.png"
+  },
+  {
+    "revision": "b60ad48da64449f36dcbb95ce72b8ee3",
+    "url": "/demo/static/media/drop.b60ad48d.png"
+  }
+]);
